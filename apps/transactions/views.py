7589d5056c5c855dc5c25fb0ff20316f96cfdfea
WalterCun <waltercunbustamante@gmail.com>
2025-03-17 12:27:07 -0500

    Add login requirement and feedback messages for transactions
    Enforced user login for transaction views using the `@login_required` decorator. Added user feedback with success and error messages when adding a transaction. Updated redirection after a successful transaction to the authentication module.

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, redirect
 from .forms import TransactionForm
 from .models import Transaction
+from django.contrib import messages
 
-
+@login_required
 def add_transaction(request):
     if request.method == "POST":
         form = TransactionForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("dashboard:home")  # Redirigir a donde se necesite
+            messages.success(request, "Transaction added successfully.")
+            return redirect("authentication:auth")
+        else:
+            messages.error(request, "Please correct the errors below.")
     else:
         form = TransactionForm()
 
     return render(request, "transactions/add_transaction.html", {"form": form})
-
+    
+@login_required
 def list_transactions(request):
     transactions = Transaction.objects.filter(account__user=request.user)
     return render(request, "transactions/list.html", {"transactions": transactions})
